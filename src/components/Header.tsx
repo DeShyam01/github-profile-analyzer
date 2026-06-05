1820a7ce78c122ae38a9b9921b3ec6d5814f7555
DeShyam01 <shyamsundargayen345@gmail.com>
2026-06-05 08:59:08 +0530

    Searchbar added and refactored css color palette

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "@/components/ThemeToggle";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between w-full h-16 px-4 bg-surface text-theme border-theme">
+    <div className="flex items-center justify-between w-full h-16 px-4 border-theme elevated">
       <h1 className="text-xl font-bold flex align-middle justify-center">Github Analyzer</h1>
       <ThemeToggle />
     </div>
