a229e4fec27b69e946e1a64ecec2b110712804b4
DeShyam01 <shyamsundargayen345@gmail.com>
2026-06-11 14:13:49 +0530

    fixed horizontal scrolling issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex flex-col">
-      <section className="min-h-screen flex flex-col">
+    <div className="w-full min-h-screen overflow-x-hidden">
+      <section className="w-full min-h-screen flex flex-col">
         <Header />
         <main className="flex-1 flex items-center justify-center">
           <GithubAnalyzer />
         </main>
       </section>
-      <Footer />
+      <Footer />  
     </div>
   );
 }
